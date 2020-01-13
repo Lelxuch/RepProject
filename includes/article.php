100016e7a4d2709ef93e0970ab95e9a180f7b9f1
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2020-01-13 09:19:18 +0600

    delete of article.php

--- includes/article.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require "config.php";
-	$article_id = 1;
-	$article = mysqli_query($connection, 'SELECT * FROM blog_article WHERE id = ' . $article_id);
-	while( $article_data = mysqli_fetch_assoc($article) ){
-?>
