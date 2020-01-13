2109fc68b1d5cf713c1b83c6c6375c6e572d10be
Lelxuch <49027400+Lelxuch@users.noreply.github.com>
2020-01-13 09:20:31 +0600

    added admin home

---
+++ assets/pages/admin/home.php
@@ -0,0 +1,15 @@
+<?php
+    include("menu.php");
+?>
+            <div class="home">
+                <div class="home_logo">
+                    <img class="home_logo_img" src="../../images/admin/logo.png">
+                </div>
+                <div class="home_name">
+                    Bilim-innovation lyceum
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
