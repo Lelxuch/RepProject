3a38cc26ff2e7711ee3fe9fa254bc2fe6813f63a
Lelxuch <49027400+Lelxuch@users.noreply.github.com>
2020-01-27 21:25:32 +0600

    added dd and reg

--- assets/pages/login.php
+++ assets/pages/login.php
@@ -9,27 +9,28 @@
 </head>
 <body>
     <div class="main">
-		<div class="form_content">
-			<form class="form" action="../../includes/login_check.php" method="post">
-				<div class="form_title">
-					Вход
+		<form class="form" action="../../includes/login_check.php" method="post">
+			<div class="form_title">
+				Вход
+			</div>
+			<div class="type">
+				<label class="type_title">Выберите тип пользователя</label>
+				<div class="sel">
+					<select name="sel_type">
+						<option value="all">-</option>
+						<option value="student">Я ученик</option>
+						<option value="teacher">Я учителя</option>
+						<option value="editor">Я редактор</option>
+					</select>
 				</div>
-				<input class="user_input" type="text" name="username" placeholder="Логин" required>
-				<input class="pswd_input" type="password" name="pswd" placeholder="Пароль" required>
-				<div class="type">
-					<div class="type_content">
-						<label class="type_title">Выберите тип пользователя</label>
-						<select class="dropdown" name="sel_type" require>
-							<option value="all">-</option>
-							<option value="student">Я ученик</option>
-							<option value="teacher">Я учитель</option>
-							<option value="editor">Я редактор</option>
-						</select>
-					</div>
-				</div>
-				<button class="submit_btn" type="submit" name="submit">Войти</button>
-			</form>
-		</div>
+			</div>
+			<input class="user_input" type="text" name="username" placeholder="Логин" required>
+			<input class="pswd_input" type="password" name="pswd" placeholder="Пароль" required>
+			<div class="footer">
+				<a class="reg_link" href="">Зарегистрируйтесь</a>
+			</div>
+			<button class="submit_btn" type="submit" name="submit">Войти</button>
+		</form>
     </div>
 </body>
 </html>
