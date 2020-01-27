b59f7cf3240c6ed5951eb3ac95b6d1c0c3616e52
Lelxuch <49027400+Lelxuch@users.noreply.github.com>
2020-01-27 20:21:31 +0600

    added dropdown

--- assets/pages/login.php
+++ assets/pages/login.php
@@ -9,16 +9,27 @@
 </head>
 <body>
     <div class="main">
-		<form class="form" action="../../includes/login_check.php" method="post">
-			<div class="form_content">
+		<div class="form_content">
+			<form class="form" action="../../includes/login_check.php" method="post">
 				<div class="form_title">
 					Вход
 				</div>
 				<input class="user_input" type="text" name="username" placeholder="Логин" required>
 				<input class="pswd_input" type="password" name="pswd" placeholder="Пароль" required>
+				<div class="type">
+					<div class="type_content">
+						<label class="type_title">Выберите тип пользователя</label>
+						<select class="dropdown" name="sel_type" require>
+							<option value="all">-</option>
+							<option value="student">Я ученик</option>
+							<option value="teacher">Я учитель</option>
+							<option value="editor">Я редактор</option>
+						</select>
+					</div>
+				</div>
 				<button class="submit_btn" type="submit" name="submit">Войти</button>
-			</div>
-		</form>
+			</form>
+		</div>
     </div>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@
                     <a class="nav_link" href="#" onclick="slowScroll('#about_link')"><?php echo $config[$lang]['header_about']; ?></a>
                     <a class="nav_link" href="#" onclick="slowScroll('#advantage_link')"><?php echo $config[$lang]['header_advantage']; ?></a>
                     <a class="nav_link" href="#" onclick="slowScroll('#admission_link')"><?php echo $config[$lang]['header_admission']; ?></a>
-                    <a class="nav_link" href="assets/pages/hz/awards.php" onclick="slowScroll('#rating_link')"><?php echo $config[$lang]['header_rating']; ?></a>
+                    <a class="nav_link" href="assets/pages/hz/awards.php"><?php echo $config[$lang]['header_rating']; ?></a>
                     <a class="nav_link" href="#" onclick="slowScroll('#event_link')"><?php echo $config[$lang]['header_event']; ?></a>
                     <a class="nav_link" href="#" onclick="slowScroll('#blog_link')"><?php echo $config[$lang]['header_blog']; ?></a>
                     <a class="nav_link" href="assets/pages/login.php" target="_blank"><img class="user_img" src="../assets/images/header/header_user.png"></a>
