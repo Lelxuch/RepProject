8b78e4fdfb554ac823393fd0c037a40f2582fe1f
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2020-01-27 21:28:29 +0600

    Update login.php

--- assets/pages/login.php
+++ assets/pages/login.php
@@ -20,7 +20,7 @@
 					<div class="type_content">
 						<label class="type_title">Выберите тип пользователя</label>
 						<select class="dropdown" name="sel_type" required>
-							<option value="all">-</option>
+							<option>-</option>
 							<option value="student">Я ученик</option>
 							<option value="teacher">Я учитель</option>
 							<option value="editor">Я редактор</option>
