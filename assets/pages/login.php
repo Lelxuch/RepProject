de761f1598f05c5952bfc07680111e1e6956c9a0
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2020-01-28 21:50:49 +0600

    xd

--- assets/pages/login.php
+++ assets/pages/login.php
@@ -1,14 +1,16 @@
 <?php require "../../includes/config.php"; ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<link rel="stylesheet" type="text/css" href="../style/style_login.css">
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Jomolhari|Roboto|Kaushan+Script|Montserrat|Open+Sans&display=swap" rel="stylesheet">
 	<meta charset="utf-8">
 	<title>Вход</title>
 </head>
+
 <body>
-    <div class="main">
+	<div class="main">
 		<form class="form" action="../../includes/login_check.php" method="post">
 			<div class="form_title">
 				Вход
@@ -27,10 +29,11 @@
 			<input class="user_input" type="text" name="username" placeholder="Логин" required>
 			<input class="pswd_input" type="password" name="pswd" placeholder="Пароль" required>
 			<div class="footer">
-				<a class="reg_link" href="">Зарегистрируйтесь</a>
+				<a class="reg_link" href="register.php">Зарегистрируйтесь</a>
 			</div>
 			<button class="submit_btn" type="submit" name="submit">Войти</button>
 		</form>
-    </div>
+	</div>
 </body>
+
 </html>
